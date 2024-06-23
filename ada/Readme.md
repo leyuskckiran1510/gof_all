cd7f75edc9294798261eaf59c922310b4d1b8af8
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-06-23 23:36:14 +0545

    feat:- GOF in 'A' for Ada

---
+++ ada/Readme.md
@@ -0,0 +1,20 @@
+# Conway's Game Of Life in Ada
+
+[image1](./imgs/1.png)
+[image2](./imgs/2.png)
+
+# How to run
+
+- if you have `make` in your system
+```
+make
+```
+
+- else compile it and run 
+
+
+# Requirements
+- linux
+    - you need `gnat` for compiling ada
+- others
+    - please research yourself ;)
---
+++ ada/gof.adb
@@ -0,0 +1,137 @@
+with Ada.Text_IO;
+with Ada.Real_Time;
+
+-- Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+-- Any live cell with two or three live neighbours lives on to the next generation.
+-- Any live cell with more than three live neighbours dies, as if by overpopulation.
+-- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
+procedure Gof is
+    mat_size:Integer := 10;
+    type Integer_Array is array(0 .. mat_size-1) of Integer;
+    type Matrix is array(0 .. mat_size-1) of Integer_Array;
+
+
+    function check(input:Matrix; x,y:Integer) return Integer  is
+        ans:Integer:=0;
+    begin -- check
+         if input(y)(x) > 0 then
+            ans := 1;
+            -- Ada.Text_IO.Put("⬜");
+        else 
+            -- Ada.Text_IO.Put("⬛");
+            null;
+        end if;
+        return ans;
+    end check;
+
+    function Alive(input : Matrix) return Matrix is
+      output : Matrix := input;
+      alive_count:Integer :=0;
+      new_x:Integer;
+      new_y:Integer;
+      begin
+          for y in input'Range loop
+                for x in input(y)'Range loop
+                    alive_count:=0;
+                    -- (-1,-1)
+                    new_x := (mat_size +x-1) mod mat_size ;
+                    new_y := (mat_size +y-1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                     -- (-1,0)
+                    new_x := (mat_size +x-1) mod mat_size ;
+                    new_y := (mat_size +y) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (-1,1)
+                    new_x := (mat_size +x-1) mod mat_size ;
+                    new_y := (mat_size +y+1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (0,-1)
+                    new_x := (mat_size +x) mod mat_size ;
+                    new_y := (mat_size +y-1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (0,1)
+                    new_x := (mat_size +x) mod mat_size ;
+                    new_y := (mat_size +y+1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (1,0)
+                    new_x := (mat_size +x+1) mod mat_size ;
+                    new_y := (mat_size +y) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (1,1)
+                    new_x := (mat_size +x+1) mod mat_size ;
+                    new_y := (mat_size +y+1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+                 -- (1,-1)
+                    new_x := (mat_size +x+1) mod mat_size ;
+                    new_y := (mat_size +y-1) mod mat_size ;
+                    alive_count := alive_count + check(input,new_x,new_y);
+                
+
+                    if alive_count < 2 or alive_count > 3 then
+                        output(y)(x) := 0;
+                    elsif alive_count = 3 then
+                        output(y)(x) := 1;
+                    elsif alive_count =2 then
+                        output(y)(x) := input(y)(x);
+
+                    end if;
+                end loop;
+                
+          end loop;
+          return output;
+    end Alive;
+
+
+
+
+
+    procedure Display(input:Matrix) is
+    begin -- Display
+        
+        -- Move Cursor to kind of center of screen vertically
+        Ada.Text_IO.Put_Line(ASCII.ESC & "[10;0H");
+        for i in input'Range loop
+                for j in input(i)'Range loop
+                    if input(i)(j) > 0 then
+                        Ada.Text_IO.Put("⬜");
+                    else 
+                        Ada.Text_IO.Put("⬛");
+                    end if;
+                end loop;
+                 Ada.Text_IO.Put_Line("");
+          end loop;
+    end Display;
+
+
+
+    input: Matrix := ((0,0,0,0,0,0,0,0,0,0),
+                      (0,0,1,0,0,0,0,1,0,0),
+                      (0,0,0,1,0,0,0,0,1,0),
+                      (0,1,1,1,0,0,1,1,1,0),
+                      (0,0,0,0,0,0,0,0,0,0),
+                      (0,0,0,0,0,0,0,0,0,0),
+                      (0,0,1,0,0,0,0,1,0,0),
+                      (0,0,0,1,0,0,0,0,1,0),
+                      (0,1,1,1,0,0,1,1,1,0),
+                      (0,0,0,0,0,0,0,0,0,0)
+                      ); 
+    use Ada.Real_Time;
+
+   Delay_Duration : constant Time_Span := Milliseconds(100); -- 1 second
+begin
+
+    for i in 1 .. 10000 loop
+       Display(input);
+       delay until Clock + Delay_Duration;
+       input := Alive(input);
+    end loop;
+end Gof;
