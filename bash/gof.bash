dd766d2c6c4f23f2e1b0ea17b2cec7d91e0b6bbf
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-06-24 23:11:26 +0545

    feat:- GOF in 'B' for Bash

---
+++ bash/gof.bash
@@ -0,0 +1,174 @@
+#  Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+#  Any live cell with two or three live neighbours lives on to the next generation.
+#  Any live cell with more than three live neighbours dies, as if by overpopulation.
+#  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
+declare -A matrix
+declare -A buffer
+
+size=15
+FRAME_COUNT=0
+
+alv="⬜"
+ded="⬛"
+
+generate(){
+    for (( i = 0; i < $size ; i++ )); do
+        for (( j = 0; j < $((size)) ; j++ )); do
+            if [[ $((RANDOM%2)) -lt 0 ]]; then
+                matrix[$i,$j]=$alv; 
+            else
+                matrix[$i,$j]=$ded; 
+            fi
+        done
+    done
+
+}
+
+
+is_alive(){ 
+    count=$3
+    if [[ $((FRAME_COUNT%2)) -eq 0 ]]; then
+        value=${matrix[$1,$2]}
+    else
+        value=${buffer[$1,$2]}
+    fi
+    # printf "%s" $value
+    case $value in
+        $alv ) return $((1+count)) ;;
+    esac
+    return $((count));
+}
+
+nextsetp(){
+      for (( i = 0; i < $size ; i++ )); do
+        for (( j = 0; j < $((size)) ; j++ )); do
+            alive_count=0;
+
+            # -1,-1
+            x=$(( (j+size-1)%size ))
+            y=$(( (i+size-1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # 0,-1
+            x=$(( (j+size)%size ))
+            y=$(( (i+size-1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # 1,-1
+            x=$(( (j+size+1)%size ))
+            y=$(( (i+size-1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # -1,0
+            x=$(( (j+size-1)%size ))
+            y=$(( (i+size)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # -1,1
+            x=$(( (j+size-1)%size ))
+            y=$(( (i+size+1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # 0,1
+            x=$(( (j+size)%size ))
+            y=$(( (i+size+1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # 1,0
+            x=$(( (j+size+1)%size ))
+            y=$(( (i+size)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+            # 1,1
+            x=$(( (j+size+1)%size ))
+            y=$(( (i+size+1)%size ))
+            is_alive $x $y $alive_count
+            alive_count=$?
+
+
+            if [[ $((FRAME_COUNT%2)) -eq 0 ]]; then
+                if [[ $alive_count -lt 2 || $alive_count -gt 3 ]]; then
+                    buffer[$j,$i]=$ded
+                elif [[ $alive_count -eq 3 ]]; then
+                    buffer[$j,$i]=$alv
+                else
+                    buffer[$j,$i]=${matrix[$j,$i]}
+                fi
+            else
+                if [[ $alive_count -lt 2 || $alive_count -gt 3 ]]; then
+                    matrix[$j,$i]=$ded
+                elif [[ $alive_count -eq 3 ]]; then
+                    matrix[$j,$i]=$alv
+                else
+                    matrix[$j,$i]=${buffer[$j,$i]}
+                fi
+            fi
+        done
+    done
+    FRAME_COUNT=$((FRAME_COUNT+1))
+}
+
+
+
+display(){
+    printf "\x1b[1J\x1b[1;1H"
+    for (( i = 0; i < $size ; i++ )); do
+        printf "\t"
+        for (( j = 0; j < $((size)) ; j++ )); do
+            if [[ $((FRAME_COUNT%2)) -eq 0 ]]; then
+               echo -n ${matrix[$i,$j]};
+            else
+               echo -n ${buffer[$i,$j]};
+            fi
+        done
+        printf "\n"
+    done
+    sleep $(echo 1/$size | bc -l )
+
+}
+glider(){
+    matrix[0,0]=$alv
+    matrix[0,2]=$alv
+    matrix[1,1]=$alv
+    matrix[1,2]=$alv
+    matrix[2,1]=$alv
+}
+
+
+box(){
+    matrix[1,0]=$alv
+    matrix[1,1]=$alv
+    matrix[1,2]=$alv
+    
+    matrix[0,0]=$alv
+    matrix[0,1]=$alv
+    matrix[0,2]=$alv
+
+    matrix[0,4]=$alv
+    matrix[1,4]=$alv
+    matrix[4,0]=$alv
+    matrix[4,1]=$alv
+    matrix[4,4]=$alv
+
+    matrix[2,0]=$alv
+    matrix[2,1]=$alv
+    matrix[2,2]=$alv
+
+}
+generate
+glider
+# box
+# while [[ $FRAME_COUNT -lt 7 ]]; do
+while [[ 1 ]]; do
+    display
+    nextsetp
+done
