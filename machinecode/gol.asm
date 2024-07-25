c95dbb71a9eb6e417fdcaa41d199e1928c43e2b3
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-07-26 00:18:33 +0545

    feat: add matrix methods
    -add matrix lookup by row,col
    - add display matrix with emojies
    - add normal 0-1 matrix display
    - add matrix insertion by row,col
    - add buffer to matrix copy

--- machinecode/gol.asm
+++ machinecode/gol.asm
@@ -1,6 +1,11 @@
 SECTION .data     
-msg:    db "000000010000010011100000",10
-len:    equ $-msg       
+matrix:    db "0000000100000100111000000",10
+buff:      db "0100000000000000000000000",10
+alive_sym: db "⬜"
+dead_sym:  db  "⬛"
+newline:   db 10
+buff_len:    equ 26    
+sym_len:     equ 3    
 width:  equ 0x5
 height: equ 0x5
                 
@@ -9,10 +14,26 @@ global main
 
 
 print_stdout:
+    push rbx
+    push rax 
     mov ebx,1       
     mov eax,4       
-    int 0x80        
+    int 0x80
+    pop rax
+    pop rbx        
     ret
+
+xytoindex:
+    ; given value at ecx(row),edx(col) -> ecx (row * width + col)
+    push rax
+    mov eax,width
+    mul ebx
+    add eax,ecx
+    mov ecx,eax
+    pop rax
+    ret
+
+
 
 exit_main:
     mov ebx,0       
@@ -20,43 +41,149 @@ exit_main:
     int 0x80
     ret
 
+print_new_line:
+    push rdx
+    push rcx 
+    mov edx,1
+    mov ecx,newline
+    call print_stdout;
+    pop rcx
+    pop rdx
+    ret
+
+
+insert_zero:
+    push rbx
+    push rcx
+    mov ebx,buff
+    add ebx,eax
+    mov ecx,0x30
+    mov [ebx],ecx
+    pop rcx
+    pop rbx
+    ret
+
+insert_one:
+    push rbx
+    push rcx
+    mov ebx,buff
+    add ebx,eax
+    mov ecx,0x31
+    mov [ebx],ecx
+    pop rcx
+    pop rbx
+    ret
+
+
+print_buff:
+    push rax
+    push rcx
+    mov edx,buff_len    
+    mov ecx,buff  
+    call print_stdout;
+    pop rcx
+    pop rax
+    ret 
 
 print_matrix:
-    mov bh,0x0; y
-    mov bl,0x0; X
-    mov ax,0x1;
-    row_begin:
-        cmp bh,height
-        jz row_end
-            call print_stdout;
-            inc bh
-            cmp bl,width
-            jz row_begin
-                call print_stdout;
-                mov AX, width    ; wdith * y + x
-                mul bh
-                add AX, bx
-                ; add AX , [ msg + 0 ]  
-                mov edx, 1
-                mov ecx,msg 
-                call print_stdout
-                inc bl
+    push rax
+    push rcx
+    mov edx,buff_len    
+    mov ecx,matrix  
+    call print_stdout;
+    pop rcx
+    pop rax
+    ret 
 
-    row_end:
+
+matat:
+    ; given ebx(row),ecx(col) -> value at buff[row][col] as ecx 
+    ; max index 255 as of now
+    call xytoindex;
+    mov ebx,matrix
+    add ebx, ecx
+    mov ecx,[ebx]
+    and ecx,0xff ; mask out last 1byte only
+    ret 
+
+
+copy_to_matrix:
+    push rax
+    mov eax,width*height;
+    mov edx,0x0
+    mov ebx,buff
+    mov ecx,matrix
+    .iloop:
+        add ebx,edx 
+        add ecx,edx
+        inc edx
+        push rax
+        mov eax,[ebx]
+        mov [ecx],eax
+        pop rax
+        dec eax
+        jnz .iloop;
+    pop rax
+    ret
+
+print_symbol:
+    cmp ecx,0x30
+    jz print_dead;
+        mov ecx,alive_sym;
+        mov edx,sym_len;
+        call print_stdout;
         ret
+    print_dead:
+        mov ecx,dead_sym;
+        mov edx,sym_len;
+        call print_stdout;
 
+    ret
 
+display_matrix:
+    ; save registers
+    push rax
+    push rbx
+    push rcx
+    push rdx
 
+    mov eax,0x0
+    .row:
+        mov ebx,0x0
+        .col:
+            push rbx
+            ;ecx -> col
+            mov ecx,ebx
+            ;ebx -> row
+            mov ebx,eax
+            call matat;
+            pop rbx
+            call print_symbol;
+            inc ebx
+            cmp ebx,width
+            jnz .col;
+        call print_new_line;
+        inc eax;
+        cmp eax,height
+        jnz .row;
+
+    ; restore registers
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    ret
 
 
 
 main:
-    mov al, 0x41           
-    mov [msg + width], al   
-    mov edx,len    
-    mov ecx,msg  
-    call print_stdout;
-    call print_matrix;
-    call exit_main
+    ; call print_buff;
+    ; call print_new_line;
+    ; call print_matrix;
+    ; call copy_to_matrix;
+    ; call print_matrix;
+    call display_matrix;
+    .end:
+        call exit_main
 
 
